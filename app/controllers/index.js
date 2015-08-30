0c36e5799bb9934531aa00c7de58bae8e9c36ccc
Narendra RaoRane <narendraraorane@Narendras-MacBook-Pro.local>
2015-08-30 19:39:14 +0530

    1. Added to do list. 2. Integrated SMS and Email functionality. 3. List the to do by desc order. 4. Add/Update functionality. 5. Search functionality. 6. Sorting the model.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,58 @@
-function doClick(e) {
-    alert($.label.text);
-}
+/**
+ * Global Navigation Handler.
+ * It handles the navigation accross the app.
+ */
+Alloy.Globals.Navigator = {
+	
+	/**
+	 * Handle to the Navigation Controller
+	 */
+	navGroup: $.nav,
+	
+	open: function(controller, payload){
+		var win = Alloy.createController(controller, payload || {}).getView();
+		
+		if(OS_IOS){
+			$.nav.openWindow(win);
+		}
+		else if(OS_MOBILEWEB){
+			$.nav.open(win);
+		}
+		else {
+			
+			// added this property to the payload to know if the window is a child
+			if (payload.displayHomeAsUp){
+				win.addEventListener('open',function(evt){
+					var activity=win.activity;
+					activity.actionBar.displayHomeAsUp=payload.displayHomeAsUp;
+					activity.actionBar.onHomeIconItemSelected=function(){
+						evt.source.close();
+					};
+				});
+			}
+			win.open();
+		}
+	},
+	close: function(win) {
+		if(OS_IOS) {
+			$.nav.closeWindow(win);
+		} else {
+			win.close();
+		}
+		
+	}
+};
 
-$.index.open();
+/**
+ * Open the window as per platform.
+ * 
+ */
+if(OS_IOS){
+	$.nav.open();
+}
+else if(OS_MOBILEWEB){
+	$.index.open();
+}
+else{
+	$.index.getView().open();
+} 
